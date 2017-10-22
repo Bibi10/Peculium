9fbbab544b114d373865cd020ec6485f7454b912
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2017-10-22 10:48:56 +0200

    mohamed : new version , burnable and safe

---
+++ contracts/v0.7/Team.sol
@@ -0,0 +1,49 @@
+/*
+This Token Contract implements the Peculium token (beta)
+.*/
+
+
+import "./Ownable.sol";
+import "./StandardToken.sol";
+pragma solidity ^0.4.8;
+
+contract Team is StandardToken,Ownable {
+	using SafeMath for uint256;
+	uint256 public START_PRE_ICO_TIMESTAMP   =1509494400; //start date of PRE_ICO 
+        uint256 public START_ICO_TIMESTAMP=START_PRE_ICO_TIMESTAMP+ 10* 1 days ;
+
+	uint256 public constant INITIAL_PERCENT_ICO_TOKEN_TO_ASSIGN = 25 ; 
+	uint256 public constant END_PAYMENTE_TIMESTAMP=1533074400;
+	uint256 public END_ICO_TIMESTAMP   =1514764800;
+	uint256 public teamShare; //token for the dev team
+	uint256 public bountyShare;
+	uint256 public bountymanagerShare;
+	uint256 public bountyFinal;
+	uint256 public dateOfPayment_TimeStamp;
+	uint256 public Airdropsamount;
+	uint256 public beginICOdate;	
+	
+	//Constructor
+	function Team(uint256 amount) {
+		teamShare=amount;
+	}
+	
+
+    /* 
+	function for paying wages of our team from the end of ico to six months after the end
+	first payement= 40% of teamShare 
+	then six payments of 10% of teamShare
+
+*/
+	function teamPayment(address teamaddr) onlyOwner{
+		if(now>dateOfPayment_TimeStamp && now< END_PAYMENTE_TIMESTAMP){
+			uint256 wages=teamShare*10/100;
+		    	if(dateOfPayment_TimeStamp<END_ICO_TIMESTAMP+ 30 * 1 days){
+		     		wages=teamShare*40/100;
+				dateOfPayment_TimeStamp+=60*1 days; //second payement two months from the first one.
+		     	}
+                     	balances[teamaddr]+=wages;
+		     	dateOfPayment_TimeStamp+=30*1 days;
+		}
+	}
+}
