c451ee9350820b06d5c332a75b76b5bbe73fde33
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2017-10-27 16:09:39 +0200

    final version (I hope)

--- contracts/v1.0/Airdrop.sol
+++ contracts/v1.0/Airdrop.sol
@@ -1,5 +1,5 @@
 /*
-Contract for the airdrop
+Contract for the airdrop of the Peculium campaign
 */
 
 
@@ -9,71 +9,90 @@ import "./Ownable.sol";
 
 import "./Peculium.sol";
 
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.15;
 
 
 contract Airdrop is Ownable{
 	using SafeMath for uint256;
 	
-	uint256 public Airdropsamount;
-	uint256 public decimals;
+	uint256 public Airdropsamount; // Airdrop total amount
+	uint256 public decimals; // decimal of the token
 	
-	Peculium public pecul;
-	bool initPecul;
+	Peculium public pecul; // token Peculium
+	bool initPecul; // We need first to init the Peculium Token address
 
-	event AirdropOne(address airdropaddress,uint256 nbTokenSendAirdrop);
-	event AirdropList(address[] airdropListAddress,uint256[] listTokenSendAirdrop);
-	event CancelledAirDrop(uint256 Total,uint256 AirdropsamountTotal);
+	event AirdropOne(address airdropaddress,uint256 nbTokenSendAirdrop); // Event for one airdrop
+	event AirdropList(address[] airdropListAddress,uint256[] listTokenSendAirdrop); // Event for all the airdrop
 	
-	address PeculiumContractAdress;
+	address PeculiumContractAdress; // address of the Peculium Token Contract
 	
+	//Constructor
 	function Airdrop(){
-		Airdropsamount = SafeMath.mul(50000000,(10**8));
+	
+		Airdropsamount = SafeMath.mul(50000000,(10**8)); // We allocate 50 Millions token for the airdrop (maybe to change) 
 		initPecul = false;
 	}
 	
-	function InitPeculiumAdress(address peculAdress) onlyOwner{
+	
+	/***  Functions of the contract ***/
+	
+	
+	function InitPeculiumAdress(address peculAdress) onlyOwner
+	{ // We init the Peculium token address
+	
 		PeculiumContractAdress = peculAdress;
 		pecul = Peculium(PeculiumContractAdress);
 		decimals = pecul.decimals();
 		initPecul = true;
+		
 	}
 	
 
-	function airdropsTokens(address[] _vaddr, uint256[] _vamounts) onlyOwner Initialize NotEmpty { // need to test the cost, need to optimize
+	function airdropsTokens(address[] _vaddr, uint256[] _vamounts) onlyOwner Initialize NotEmpty 
+	{ // need to test the cost, need to optimize
+		
 		require (Airdropsamount >0);
 		require ( _vaddr.length == _vamounts.length );
 		//Looping into input arrays to assign target amount to each given address 
 		uint256 amountToSendTotal = 0;
+		
 		for (uint256 indexTest=0; indexTest<_vaddr.length; indexTest++) // We first test that we have enough token to send
 		{
+		
 			amountToSendTotal.add(_vamounts[indexTest].mul(10 ** decimals)); 
+		
 		}
 		
-		require(amountToSendTotal>Airdropsamount); // If no enough token, cancel the sell 
+		require(amountToSendTotal<=Airdropsamount); // If no enough token, cancel the sell 
 		
-		for (uint256 index=0; index<_vaddr.length; index++) {
+		for (uint256 index=0; index<_vaddr.length; index++) 
+		{
+			
 			address toAddress = _vaddr[index];
 			uint256 amountTo_Send = _vamounts[index].mul(10 ** decimals);
 		
-	                pecul.transfer(toAddress,amountTo_Send); // maybe doesn't work because of owner, need tests
+	                pecul.transfer(toAddress,amountTo_Send); // need tests
 			
 			Airdropsamount.sub(amountTo_Send);
 			AirdropOne(toAddress,amountTo_Send);
+			
 		}
+			
 		AirdropList(_vaddr,_vamounts);
 	      
-		}
+	}
 	
+	/***  Modifiers of the contract ***/
 
-		modifier NotEmpty {
+	modifier NotEmpty {
 		require (Airdropsamount>0);
 		_;
-    		}
-    		modifier Initialize {
-		require (initPecul==true);
-		_;
-    	} 
+	}
+	
+	modifier Initialize {
+	require (initPecul==true);
+	_;
+	} 
 
     
     }
--- contracts/v1.0/BountyManager.sol
+++ contracts/v1.0/BountyManager.sol
@@ -1,41 +1,50 @@
 /*
-This Token Contract pay the bounty holder and the airdrop
+This Token Contract pay the bounty holder of the Peculium campaign
 .*/
 import "./SafeMath.sol";
 import "./Ownable.sol";
 
 import "./Peculium.sol";
 
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.15;
 
 contract BountyManager is Ownable  {
 	using SafeMath for uint256;
-	uint256 public bountyShare;
-	uint256 public bountymanagerShare;
-	uint256 public bountyRemaining;
 
-	bool First_pay_bountymanager;
 	
-	bool initPecul;
-	bool bountyInit;
-	uint256 first_pay;
-	uint256 montly_pay;
-	
-	uint256 payday;
-	
-	Peculium public pecul;
 
-	address bountymanager ; // public key of the bounty manager 
 	
-	address PeculiumContractAdress;
-//contract_address.call(bytes4(sha3("function_name(types)")),parameters_values) To call a contract
+	/*Variables about the token contract */	
+	Peculium public pecul; // The Peculium token
+	address PeculiumContractAdress; // address of the Peculium token address
+	bool initPecul; // boolean to know if the Peculium token address has been init
+	
+	event InitializedToken(address contractToken);
+	
+	/*Variables about the bounty manager */
+	address bountymanager ; // address of the bounty manager 
+	uint256 public bountymanagerShare; // nb token for the bountymanager
+	bool First_pay_bountymanager; // boolean to test if the first pay has been send to the bountymanager
+	uint256 first_pay; // pourcent of the first pay rate
+	uint256 montly_pay; // pourcent of the montly pay rate
+	bool bountyInit; // boolean to know if the bounty address has been init
+	uint256 payday; // Day when the bounty manager is paid
+	uint256 nbMonthsPay; // The montly pay is sent for 6 months
 
-
+	event InitializedManager(address ManagerAdd);
+	event FirstPaySend(uint256 first,address receiver);
+	event MonthlyPaySend(uint256 monthPay,address receiverMonthly);
+	
+	
 	//Constructor
 	function BountyManager() {
-		bountymanagerShare = SafeMath.mul(72000000,(10**8)); // we allocate 72 million token to the bounty manager
-		first_pay = SafeMath.div(SafeMath.mul(40,bountymanagerShare),100);
-		montly_pay = SafeMath.div(SafeMath.mul(10,bountymanagerShare),100);
+		
+		bountymanagerShare = SafeMath.mul(72000000,(10**8)); // we allocate 72 million token to the bounty manager (maybe to change)
+		
+		first_pay = SafeMath.div(SafeMath.mul(40,bountymanagerShare),100); // first pay is 40%
+		montly_pay = SafeMath.div(SafeMath.mul(10,bountymanagerShare),100); // other pay are 10%
+		nbMonthsPay = 0;
+		
 		First_pay_bountymanager=true;
 		initPecul = false;
 		bountyInit==false;
@@ -43,44 +52,62 @@ contract BountyManager is Ownable  {
 
 	}
 	
-	function InitPeculiumAdress(address peculAdress) onlyOwner{
+	
+	/***  Functions of the contract ***/
+	
+	function InitPeculiumAdress(address peculAdress) onlyOwner 
+	{ // We init the address of the token
+	
 		PeculiumContractAdress = peculAdress;
 		pecul = Peculium(PeculiumContractAdress);
 		payday = pecul.dateDefrost();
 		initPecul = true;
+		InitializedToken(PeculiumContractAdress);
+	
 	}
 	
-	function change_bounty_manager (address public_key) onlyOwner{ // to change the bounty manager
+	function change_bounty_manager (address public_key) onlyOwner 
+	{ // to change the bounty manager address
+	
 		bountymanager = public_key;
 		bountyInit=true;
+		InitializedManager(public_key);
+	
 	}
 	
-	function allocateManager() onlyOwner Initialize BountyManagerInit { // Allocate pecul for the Bounty manager, after this call, the manager can take the token by calling transferFrom 
+	function allocateManager() onlyOwner Initialize BountyManagerInit 
+	{ // Allocate pecul for the Bounty manager, after this call, the manager can take the token by calling transferFrom 
+		
 		require(now > payday);
+		
 		if(First_pay_bountymanager==true)
 		{
 
+			pecul.approve(bountymanager,first_pay);
+			payday = payday + 4 weeks;
+			First_pay_bountymanager==false;
+			FirstPaySend(first_pay,bountymanager);
 		
-		pecul.approve(bountymanager,first_pay);
-		
-		payday = payday + 4 weeks;
-		First_pay_bountymanager==false;
 		}
-		if(First_pay_bountymanager==false)
-		{
-		pecul.approve(bountymanager,montly_pay);
 
-		payday = payday + 4 weeks;
+		if(First_pay_bountymanager==false && nbMonthsPay < 6)
+		{
+
+			pecul.approve(bountymanager,montly_pay);
+			payday = payday + 4 weeks;
+			nbMonthsPay.add(1);
+			MonthlyPaySend(montly_pay,bountymanager);
 		
 		}
 		
 	}
+		/***  Modifiers of the contract ***/
 	
-	modifier Initialize {
+	modifier Initialize { // We need to initialize first the token contract
 		require (initPecul==true);
 		_;
     	}
-    	modifier BountyManagerInit {
+    	modifier BountyManagerInit { // We need to initialize first the address of the bountyManager
 		require (bountyInit==true);
 		_;
     	} 
