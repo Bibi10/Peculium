54ba3c0b49afaec47cc8c9e68bd9fd2f93a4eeb5
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2017-10-31 10:45:00 +0100

    Bounty solve bug

--- contracts/v1.0/BountyManager.sol
+++ contracts/v1.0/BountyManager.sol
@@ -80,7 +80,7 @@ contract BountyManager is Ownable  {
 		{
 
 			pecul.transfer(bountymanager,first_pay);
-			payday = payday + 4 weeks;
+			payday = payday.add( 4 weeks);
 			First_pay_bountymanager==false;
 			FirstPaySend(first_pay,bountymanager);
 		
@@ -90,8 +90,8 @@ contract BountyManager is Ownable  {
 		{
 
 			pecul.transfer(bountymanager,montly_pay);
-			payday = payday + 4 weeks;
-			nbMonthsPay+=1;
+			payday = payday.add( 4 weeks);
+			nbMonthsPay=nbMonthsPay.add(1);
 			MonthlyPaySend(montly_pay,bountymanager);
 		
 		}
