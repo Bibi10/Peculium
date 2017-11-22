a3d3b0978165a4ac6beba9681d1f012302d8c8ba
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2017-11-22 13:11:27 +0100

    add refund mecanism

--- contracts/v1.1/PrivateSale.sol
+++ contracts/v1.1/PrivateSale.sol
@@ -9,6 +9,9 @@ Need to add :
 */
 
 import "./Peculium.sol";
+import "./RefundVault.sol";
+
+
 
 pragma solidity ^0.4.15;
 
@@ -26,30 +29,44 @@ contract PrivateSale is Ownable{
  	// minimum amount of funds to be raised in weis
 	uint256 public goal;
 
-
-	//Boolean to allow or not the sale of tokens
-	bool public sales_stopped;	
-	
-	
 	mapping (address => uint256) PrivateSaleEthers; // for now I use a mapping, will be changed with database access
 	
 	uint256 rate; // 1 ether = 25 000 PCL, maybe to change
 	
+	// start and end timestamps where investments are allowed (both inclusive)
+ 	uint256 public startTime;
+  	uint256 public endTime;
+
+	  // address where funds are collected
+	  address public wallet;
+ 	 // amount of raised money in wei
+  	uint256 public weiRaised;
+	
+	// refund vault used to hold funds while crowdsale is running
+  	RefundVault public vault;
+
+  	bool public isFinalized = false;
 	event Finalized();
-	event Stopsale();
-	event Restartsale();
+
   	
   	event PrivateSalesSale(address receiverToken,uint256 nbTokenSend);
-	
-	event TakeEther(uint256 nb);
 	
 	event InitializedToken(address contractToken);
 	//Constructor
 	function PrivateSale(){
 		rate = 25000;
-		sales_stopped = false;
 		PrivateSalesAmount = 1000000000; // We allocate 1 Billion token for the privatesale (maybe to change) 
 		initPecul = false;
+		
+		startTime = now; // to change
+    		endTime = startTime + 10 days; //investors have 10 days to confirm their transactions
+    		wallet = owner;
+    		
+    		goal = 300000 ether;
+		
+		vault = new RefundVault(wallet);
+
+
 	}
 	
 	
@@ -67,53 +84,76 @@ contract PrivateSale is Ownable{
 	
 	function() payable {
 	
-		BuyTokens();
+		buyTokens(msg.sender);
 	
 	}
 	
-	function BuyTokens() payable NotEmpty Initialize SaleNotStopped{ // do we need to make it internal or not ?
+	function buyTokens(address beneficiary) payable NotEmpty Initialize SaleNotStopped{ // do we need to make it internal or not ?
+	    	require(beneficiary != address(0));
+    		require(now >= startTime && now <= endTime);
+    		require(msg.value != 0); // In fact more, to determine
+
+
 		require(PrivateSaleEthers[msg.sender]>0);
 		require(msg.value <= PrivateSaleEthers[msg.sender]); // to replace with database access
 		
 		uint256 tokenToSend = (msg.value).mul(rate);
 		
 		require(tokenToSend <= PrivateSalesAmount); 
-		
+		weiRaised = weiRaised.add(msg.value);
+
 		pecul.transfer(msg.sender,tokenToSend*10**decimals);
 		PrivateSaleEthers[msg.sender] = PrivateSaleEthers[msg.sender].sub(msg.value); 
 		PrivateSalesAmount = PrivateSalesAmount.sub(tokenToSend);
 		PrivateSalesSale(msg.sender,tokenToSend);
 		
-	}
-	
-	
-	function Send_ether(uint256 nbEther) onlyOwner {
-	
-		owner.transfer(nbEther * 1 ether);
-		TakeEther( nbEther * 1 ether);
-	
-	}
-	
-	function Send_All() onlyOwner {
-	
-		owner.transfer(this.balance);
-		TakeEther(this.balance);
-	
-	}
+		forwardFunds();
 
-	function stopSale() onlyOwner
-	{
-		require ( sales_stopped == false);
-		sales_stopped = true;
-		Stopsale();
 	}
+	  function forwardFunds() internal {
+    		vault.deposit.value(msg.value*9/10)(msg.sender);
+    		wallet.transfer(msg.value/10);
+  		}
+
+
+	  // if crowdsale is unsuccessful, investors can claim refunds here
+	  function claimRefund() public {
+	    require(isFinalized);
+	    require(!goalReached());
+
+	    vault.refund(msg.sender);
+	  }
 	
-	function restartSale() onlyOwner 
-	{
-      		require ( sales_stopped == true);
-      		sales_stopped = false;
-      		Restartsale();
-	}
+
+
+
+	  function goalReached() public constant returns (bool) {
+    	return (weiRaised / 1 ether) >= goal;
+  	}
+
+		  function finalize() onlyOwner public {
+	    require(!isFinalized);
+	    require(now > endTime);
+
+	    finalization();
+	    Finalized();
+
+	    isFinalized = true;
+	  }
+
+  // vault finalization task, called when owner calls finalize()
+
+	  function finalization() internal {
+	  
+	    if (goalReached()) {
+	      vault.close();
+	    } else {
+	      vault.enableRefunds();
+	    }
+
+	  }
+
+	
 	
 	/***  Modifiers of the contract ***/
 
@@ -128,9 +168,11 @@ contract PrivateSale is Ownable{
 	} 
 	
 	modifier SaleNotStopped {
-        require (!sales_stopped);
+        require (!isFinalized);
         _;
     	}
+    	
+    	
     
   }
 
